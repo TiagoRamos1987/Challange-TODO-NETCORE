38c8c9ddbee211e17cb102e84ebc58f43ca81134
Tiago Ramos <32303649+TiagoRamos1987@users.noreply.github.com>
2020-10-05 19:13:53 -0300

    APi RESTFull
    .NET Core Framework

---
+++ Model/Users.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challange_TODO_NETCORE.Model
+{
+    public class Users
+    {
+    }
+}
